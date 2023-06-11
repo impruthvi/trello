c4224702a1fc83f4bb1432186712102b95e79936
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-06-11 12:44:46 +0530

    created a header component
    Added a logo, search box and avatar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import Image from 'next/image'
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
     <main>
+      {/* Header */}
+      <Header />
+
+      {/* Board */}
       <h1>Trallo 2.0</h1>
     </main>
   )
